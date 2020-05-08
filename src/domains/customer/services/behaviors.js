764c7a5a2c61409253bc65540149852975ec784c
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-08 15:44:03 +0200

    feat(customer): added method to log a random customer in

---
+++ src/domains/customer/services/behaviors.js
@@ -0,0 +1,53 @@
+import {getGeneratedAccount, getRandomRegisteredUser} from './resources';
+import {login, register} from '../../auth/services/resources';
+
+export async function makeCustomerArrival(presentAccounts) {
+  const rng = Math.random();
+  if (rng <= 0.65) {
+    return registerNewCustomer();
+  } else {
+    const customer = await loginRandomAccount();
+    const existingCustomer = presentAccounts.find(account => account.email === customer.email);
+
+    if (existingCustomer) {
+      return makeCustomerArrival(presentAccounts);
+    }
+
+    return customer;
+  }
+}
+
+async function registerNewCustomer() {
+  const randomAccount = await getGeneratedAccount();
+  const {results: [{name: {first, last}, email}]} = randomAccount;
+  const password = `${first}${last}`;
+
+  const account = await register({
+    firstName: first,
+    lastName: last,
+    email,
+    password
+  });
+
+  const {token} = await login(email, password);
+  return {
+    ...account,
+    token
+  };
+}
+
+async function loginRandomAccount() {
+  const randomAccount = await getRandomRegisteredUser();
+  if (!randomAccount) {
+    return registerNewCustomer();
+  }
+
+  const {email, firstName, lastName} = randomAccount;
+  const password = `${firstName}${lastName}`;
+
+  const {token} = await login(email, password);
+  return {
+    ...randomAccount,
+    token
+  };
+}
