1b6cc7aaeb82459391feed7a7b0262df5ce46bd0
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-18 14:52:20 +0200

    feat(customer): customer fetches shopping list on login

--- src/domains/simulator/components/Simulator.js
+++ src/domains/simulator/components/Simulator.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import useStoreConfig from '../useStoreConfig';
 import Shop from './shop/Shop';
+import {makeCustomerArrival} from '../../customer/services/behaviors';
+import {findEntranceCoordinates} from '../services/behaviors';
 import styled from 'styled-components';
-import useCustomerSimulation from '../useCustomerSimulation';
+import {getShoppingList} from '../../product/services/resources';
 
 const ShopContainer = styled.div`
   display: flex;
@@ -11,7 +13,37 @@ const ShopContainer = styled.div`
 
 function Simulator() {
   const {shopConfig, loading} = useStoreConfig();
-  const {customers} = useCustomerSimulation();
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (customers.length === 1) {
+        return;
+      }
+
+      const customer = await makeCustomerArrival(customers);
+      const generatedShoppingList = await getShoppingList(customer.profile);
+      const computedShoppingList = generatedShoppingList.map(product => ({
+        ...product,
+        status: 'pending'
+      }));
+      const [row, col] = findEntranceCoordinates();
+
+      setCustomers([
+        ...customers,
+        {
+          customer,
+          shoppingList: computedShoppingList,
+          coordinates: {
+            row,
+            col
+          }
+        }
+      ]);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [customers]);
 
   if (loading) {
     return (
