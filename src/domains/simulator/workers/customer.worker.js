f513484438a346a5884002d7a8e9302cda34f2b3
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-18 17:21:09 +0200

    refacto(customer shopping): extracted methods in worker

--- src/domains/simulator/workers/customer.worker.js
+++ src/domains/simulator/workers/customer.worker.js
@@ -11,31 +11,39 @@ onmessage = function(event) {
   }
 }
 
+function mapCustomerToNextPath(row, col, customer, targetIndex) {
+  const targetPath = computePathToProduct(row, col, customer.shoppingList[targetIndex].name);
+
+  return {
+    ...customer,
+    travelling: {
+      ...customer.travelling,
+      path: targetPath
+    }
+  };
+}
+
+function mapCustomerWhenWalkingTowardsTarget(path, customer) {
+  const [nextCol, nextRow] = path[0];
+  const newPath = path.slice(1);
+  return {
+    ...customer,
+    travelling: {
+      ...customer.travelling,
+      path: newPath,
+      col: nextCol,
+      row: nextRow
+    }
+  };
+}
+
 setInterval(() => {
   const updatedCustomers = customers.map(customer => {
     const {row, col, targetIndex, path} = customer.travelling;
     if (path === null) {
-      const targetPath = computePathToProduct(row, col, customer.shoppingList[targetIndex].name);
-
-      return {
-        ...customer,
-        travelling: {
-          ...customer.travelling,
-          path: targetPath
-        }
-      }
+      return mapCustomerToNextPath(row, col, customer, targetIndex);
     } else if (path.length > 0) {
-      const [nextCol, nextRow] = path[0];
-      const newPath = path.slice(1);
-      return {
-        ...customer,
-        travelling: {
-          ...customer.travelling,
-          path: newPath,
-          col: nextCol,
-          row: nextRow
-        }
-      };
+      return mapCustomerWhenWalkingTowardsTarget(path, customer);
     }
     return customer;
   });
