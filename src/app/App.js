68c663b80071868db188088f3c203a583d6b57e6
Corentin <corentin.delannoy@supinfo.com>
2020-05-20 09:10:38 +0200

    feat(simulator): added functions to simulator hook to control running state (#7)
    * feat(simulator): added functions to simulator hook to control running state
    
    * feat(simulator): added stop function to simulator hook
    
    * feat(simulator): added buttons to control the simulator
    
    * fix: added customers in useEffect dependency array
    
    Co-authored-by: Corentin Delannoy <corentin.delannoy2@gmail.com>

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Simulator from '../domains/simulator/components/Simulator';
+import styled from 'styled-components';
+
+const RootContainer = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <div>
+    <RootContainer>
       <Simulator/>
-    </div>
+    </RootContainer>
   );
 }
 
--- src/domains/simulator/components/Simulator.js
+++ src/domains/simulator/components/Simulator.js
@@ -3,15 +3,50 @@ import useStoreConfig from '../useStoreConfig';
 import Shop from './shop/Shop';
 import styled from 'styled-components';
 import useCustomerSimulation from '../useCustomerSimulation';
+import Button from '../../../components/Button';
+import {faPause, faPlay, faStop} from '@fortawesome/free-solid-svg-icons';
+
+const SimulatorContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: stretch;
+  
+  & > * {
+    margin: 12px 0;
+  }
+`;
 
 const ShopContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
 
+const ActionsContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  
+  & > * {
+    &:not(:first-child) {
+      margin-left: 4px;
+    }
+    
+    &:not(:last-child) {
+      margin-right: 4px;
+    }
+  }
+`;
+
 function Simulator() {
   const {shopConfig, loading} = useStoreConfig();
-  const {customers} = useCustomerSimulation();
+  const {
+    customers,
+    isRunning,
+    toggle,
+    stop
+  } = useCustomerSimulation();
 
   if (loading) {
     return (
@@ -20,10 +55,21 @@ function Simulator() {
   }
 
   return (
-    <ShopContainer>
-      <Shop rowsConfig={shopConfig}
-            customers={customers}/>
-    </ShopContainer>
+    <SimulatorContainer>
+      <ActionsContainer>
+        <Button type="button" onClick={toggle}
+                icon={isRunning ? faPause : faPlay}>
+          {isRunning ? 'Pause' : 'Play'}
+        </Button>
+        <Button type="button" onClick={stop} icon={faStop}>
+          Stop
+        </Button>
+      </ActionsContainer>
+      <ShopContainer>
+        <Shop rowsConfig={shopConfig}
+              customers={customers}/>
+      </ShopContainer>
+    </SimulatorContainer>
   );
 }
 
