499d59dee7ababbcf0201608bb988bf976e18b2a
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-22 17:58:53 +0200

    feat(product refill): added refill call when product is under stock threshold

--- src/domains/simulator/workers/store-config.worker.js
+++ src/domains/simulator/workers/store-config.worker.js
@@ -1,18 +1,26 @@
-import {getAllProducts} from '../../product/services/resources';
+import {getAllProducts, refillProduct} from '../../product/services/resources';
 import STORE_CONFIG from '../../../lib/mocks/storeConfiguration.json';
 
 function scheduleReconfiguration() {
-  setTimeout(startReconfiguration, 15000);
+  setTimeout(startReconfiguration, 4000);
 }
 
 async function startReconfiguration() {
   const products = await getAllProducts();
+  const mappedProducts = await Promise.all(products.map(product => {
+    const threshold = Math.floor(product.maxStock * 0.25);
+    if (product.currentStock <= threshold) {
+      return refillProduct(product.name);
+    }
+    return product;
+  }));
+
   const builtStoreConfig = await STORE_CONFIG.map(rowConfig => rowConfig
     .map(cellConfig => {
       if (cellConfig.type !== 'product') {
         return cellConfig;
       }
-      const product = products.find(({name}) => name === cellConfig.product);
+      const product = mappedProducts.find(({name}) => name === cellConfig.product);
       return {
         ...cellConfig,
         product
